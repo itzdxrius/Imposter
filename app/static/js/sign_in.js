176773367b7e07b22b7675b9d312b3a2956cabc7
Darius Green <dai@Dariuss-MacBook-Air.local>
2026-07-07 17:18:58 -0400

    core game front end html pages created and integrated

--- app/static/js/sign_in.js
+++
@@ -1,5 +0,0 @@
-function handleLogin() {
-    window.location.href = "/auth/login";
-}
-const login_button = document.getElementById("login-button");
-login_button.addEventListener("click", handleLogin);
